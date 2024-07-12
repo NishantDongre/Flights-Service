3451d15deb73341089ddc1cc185c05a9dc9be5b0
Nishant <nishantdongre30@gmail.com>
2024-07-12 23:10:50 +0530

    Added flights model with foreign key constraint defined during model definition ``` npx sequelize model:generate --name Flight --attributes flightNumber:string,airplaneId:integer,departureAirportId:integer,arrivalAirportId:integer,arrivalTime:date,departureTime:date,price:integer,boardingGate:string,totalSeats:integer ```
    ```
    SQL to find Relation between tables
    select * from INFORMATION_SCHEMA.KEY_COLUMN_USAGE where TABLE_NAME = 'flights' AND CONSTRAINT_SCHEMA = 'flights';
    ```

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.use(express.text());
 
 app.use("/api", apiRoutes);
 
-app.listen(ServerConfig.PORT, async () => {
+app.listen(ServerConfig.PORT, () => {
     console.log(
         `Successfully started the server on PORT : ${ServerConfig.PORT}`
     );
